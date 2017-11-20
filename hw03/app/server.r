1e5ef767d7563ec9c9265dbbb8904f629428145f
vibudh2209 <vibudh2209@gmail.com>
2017-11-19 16:13:51 -0800

    Final app

--- hw03/app/server.r
+++ hw03/app/server.r
@@ -1,28 +1,46 @@
-library("shiny")
+library(shiny)
 library(ggplot2)
 library(dplyr)
+library(tidyverse)
 bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
 server <- function(input, output) {
 	filtered <- reactive({
-		if (is.null(input$countryInput)) {
-			return(NULL)
-		}    
 		bcl %>%
 			filter(Price >= input$PriceInput[1],
 						 Price <= input$PriceInput[2],
-						 Type == input$typeInput,
-						 Country == input$countryInput
-			)
+						 Type %in% input$typeInput) %>% 
+			{if(input$Order) arrange(.,Price) else .} %>% 
+			{if(input$OrderSw) arrange(.,Sweetness) else .} %>%
+			{if(input$Cont) filter(.,Country %in% input$countryInput) else .} %>% 
+			{if(input$Sub) filter(.,Subtype %in% input$Subtypeinput) else .}
 	})
 	output$countryOutput <- renderUI({
 		selectInput("countryInput", "Country",
-								sort(unique(bcl$Country)),
+								sort(unique(filter(bcl,
+																	 Type %in% input$typeInput,
+																	 Price >= input$PriceInput[1],
+																	 Price <= input$PriceInput[2])$Country)),multiple = TRUE,
 								selected = "CANADA")
+	})
+	output$Subtypel<- renderUI({
+		selectInput("Subtypeinput","Subtypes",
+								unique(filter(bcl,
+															Type %in% input$typeInput,
+															Price >= input$PriceInput[1],
+															Price <= input$PriceInput[2])$Subtype),selected= "NULL")
 	})
 	output$PriceOutput <- renderUI({
 		sliderInput("PriceInput", "Price Range", min = 0,
 								max = input$num, value = c(25,50),pre="$")
 	})
+	output$Downloaddata <- downloadHandler(
+		filename=function(){
+			paste("data_",input$PriceInput[1],"_",input$PriceInput[2],".csv")
+			},
+		content = function(con){
+			write.csv(filtered(),con)
+			}
+		)
 	output$coolplot <- renderPlot({
 		if (is.null(filtered())) {
 			return()
@@ -30,7 +48,7 @@ server <- function(input, output) {
 		ggplot(filtered(), aes(Alcohol_Content)) +
 			geom_histogram()
 	})
-	output$results <- renderTable({
+	output$results <- DT::renderDataTable({
 		filtered()
 	})
 }
--- hw03/app/ui.r
+++ hw03/app/ui.r
@@ -1,19 +1,35 @@
 ui <- fluidPage(
-	titlePanel("Bc Liquor Store prices"),
-	sidebarLayout(
+	div(img(src = "bc-liq.jpg",height=200),style="text-align: center;"),
+	div(img(src = "bc-liq2.jpeg",height=200),style="text-align: center;"),
+		sidebarLayout(
 		sidebarPanel(
-			radioButtons("typeInput", "Product type", 
+			checkboxGroupInput("typeInput", "Product type", 
 									 choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"), 
 									 selected = "WINE"),
-			uiOutput("countryOutput"),
-			numericInput("num", "Maximum slider value", 100),
-			uiOutput("PriceOutput")
+			checkboxInput("Sub","Filter by Subtypes",FALSE),
+			conditionalPanel(
+				condition="input.Sub==1",
+				uiOutput("Subtypel")
+			),
+			checkboxInput("Cont","Filter by Country",FALSE),
+			conditionalPanel(
+				condition="input.Cont==1",
+				uiOutput("countryOutput")
+			),
+			numericInput("num", "Maximum price range", 100),
+			uiOutput("PriceOutput"),
+			checkboxInput("Order","Increasing Price order",FALSE),
+			conditionalPanel(
+				condition="input.typeInput=='WINE'",
+				checkboxInput("OrderSw","Increasing swetness order",FALSE)
+			),
+			downloadButton("Downloaddata","Download liqours in specified input")
 		),
 		mainPanel(
-			plotOutput("coolplot"),
-			br(),
-			br(),
-			tableOutput("results")
+			tabsetPanel(
+				tabPanel("Alcohol Content",plotOutput("coolplot")),
+				tabPanel("All results Tabulated",DT::dataTableOutput("results"))
+			)
 		)
 	)
 )
